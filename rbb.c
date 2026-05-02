7eb38622fab9bf26cc337fe6e66052cd6f999a0a
Mike Klein <mtklein@gmail.com>
2026-05-02 18:26:09 -0500

    eval with evalv

--- rbb.c
+++ rbb.c
@@ -1,5 +1,4 @@
 #include "rbb.h"
-#include <math.h>
 
 enum {
     ABS=1, NEG, SQRT, FLOOR, CEIL, TRUNC, ROUND,
@@ -71,10 +70,8 @@ int rbb_inline(struct rbb *dst, struct rbb const *src, int const args[]) {
 
 typedef int v8i __attribute__((ext_vector_type(8)));
 
-static uint32_t f2u(float    x) { return __builtin_bit_cast(uint32_t, x); }
-static float    u2f(uint32_t x) { return __builtin_bit_cast(float, x); }
-static v8i      vf2u(v8f x) { return __builtin_bit_cast(v8i, x); }
-static v8f      vu2f(v8i x) { return __builtin_bit_cast(v8f, x); }
+static v8i vf2u(v8f x) { return __builtin_bit_cast(v8i, x); }
+static v8f vu2f(v8i x) { return __builtin_bit_cast(v8f, x); }
 
 void evalv(struct rbb const *rbb, v8f reg[64], struct rbb const *const call[64]) {
     for (int i = rbb->in; i < rbb->insts; i++) {
@@ -138,60 +135,12 @@ void evalv(struct rbb const *rbb, v8f reg[64], struct rbb const *const call[64])
 }
 
 void eval(struct rbb const *rbb, float reg[64], struct rbb const *const call[64]) {
+    v8f vreg[64];
+    for (int j = 0; j < rbb->in; j++) {
+        vreg[j] = reg[j];
+    }
+    evalv(rbb, vreg, call);
     for (int i = rbb->in; i < rbb->insts; i++) {
-        inst const inst = {.bits=rbb->inst[i]};
-
-        if (inst.imm <= inst.imm) {
-            reg[i] = inst.imm;
-        } else switch (inst.op) {
-            default: __builtin_unreachable();
-
-            case   ABS: reg[i] =  fabsf(reg[inst.x]); break;
-            case   NEG: reg[i] =      -(reg[inst.x]); break;
-            case  SQRT: reg[i] =  sqrtf(reg[inst.x]); break;
-            case FLOOR: reg[i] = floorf(reg[inst.x]); break;
-            case  CEIL: reg[i] =  ceilf(reg[inst.x]); break;
-            case TRUNC: reg[i] = truncf(reg[inst.x]); break;
-            case ROUND: reg[i] = roundf(reg[inst.x]); break;
-
-            case ADD: reg[i] = reg[inst.x] + reg[inst.y]; break;
-            case SUB: reg[i] = reg[inst.x] - reg[inst.y]; break;
-            case MUL: reg[i] = reg[inst.x] * reg[inst.y]; break;
-            case DIV: reg[i] = reg[inst.x] / reg[inst.y]; break;
-            case MIN: reg[i] = fminf(reg[inst.x], reg[inst.y]); break;
-            case MAX: reg[i] = fmaxf(reg[inst.x], reg[inst.y]); break;
-            case FMA: reg[i] = fmaf(reg[inst.x], reg[inst.y], reg[inst.z]); break;
-
-        #pragma GCC diagnostic push
-        #pragma GCC diagnostic ignored "-Wfloat-equal"
-            case EQ:  reg[i] = u2f(reg[inst.x] == reg[inst.y] ? ~0u : 0u); break;
-            case NE:  reg[i] = u2f(reg[inst.x] != reg[inst.y] ? ~0u : 0u); break;
-            case LT:  reg[i] = u2f(reg[inst.x] <  reg[inst.y] ? ~0u : 0u); break;
-            case LE:  reg[i] = u2f(reg[inst.x] <= reg[inst.y] ? ~0u : 0u); break;
-        #pragma GCC diagnostic pop
-
-            case AND: reg[i] = u2f( f2u(reg[inst.x]) & f2u(reg[inst.y])); break;
-            case OR : reg[i] = u2f( f2u(reg[inst.x]) | f2u(reg[inst.y])); break;
-            case NOT: reg[i] = u2f(~f2u(reg[inst.x])); break;
-            case SEL: {
-                uint32_t m = f2u(reg[inst.x]);
-                reg[i] = u2f((m & f2u(reg[inst.y])) | (~m & f2u(reg[inst.z])));
-            } break;
-
-            case CALL: {
-                struct rbb const *sub = call[inst.x];
-                int   const in  = sub->in;
-                int   const out = sub->out;
-                float subreg[64] = {0};
-                for (int j = 0; j < in; j++) {
-                    subreg[j] = reg[i - in + j];
-                }
-                eval(sub, subreg, call);
-                for (int j = 0; j < out; j++) {
-                    reg[i + j] = subreg[sub->insts - out + j];
-                }
-                i += out - 1;
-            } break;
-        }
+        reg[i] = vreg[i][0];
     }
 }
